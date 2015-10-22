ca619267bff48ee57b27a8907d270f87f9ec5f43
sushilthe <sushilthe@gmail.com>
2015-10-22 13:06:00 +0545

    Sign Up Validation

--- User Management/SignUpViewController.swift
+++ User Management/SignUpViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var profilePictureView: UIImageView!
+    @IBOutlet weak var emailAddressField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
     
     
     override func viewDidLoad() {
@@ -36,12 +41,49 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-        profilePicture.image = image
+        profilePictureView.image = image
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func cancel(sender: UIBarButtonItem) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func signUp(sender: UIButton) {
+        let emailAddress = emailAddressField.text
+        let password = passwordField.text
+        let confirmPassword = confirmPasswordField.text
+        let firstName = firstNameField.text
+        let lastName = lastNameField.text
+        
+        
+        if(emailAddress?.isEmpty ?? true || password?.isEmpty ?? true || confirmPassword?.isEmpty ?? true || firstName?.isEmpty ?? true || lastName?.isEmpty ?? true){
+            
+            validationAlert("All Fields are required")
+            return
+        }
+        
+        if(password != confirmPassword){
+            validationAlert("Passwords do not match")
+            return
+        }
+        
+        if(profilePictureView != nil){
+            let profilePicture = UIImageJPEGRepresentation(profilePictureView.image!, 1)
+            if(profilePicture != nil){
+                // TODO: Create PFFile Object and send to parse
+            }
+        }
+    }
+    
+    func validationAlert(message: String){
+        let validationAlert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let alertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        
+        validationAlert.addAction(alertAction)
+        
+        self.presentViewController(validationAlert, animated: true, completion: nil)
     }
     
 }
