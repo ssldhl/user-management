e8ac939db90fa14e5d1cfe5a317336011c52a6bf
sushilthe <sushilthe@gmail.com>
2015-10-23 11:36:03 +0545

    Forgot Password UI

---
+++ User Management/ForgotPasswordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ForgotPasswordViewController.swift
+//  User Management
+//
+//  Created by Sushil Dahal on 10/23/15.
+//  Copyright © 2015 Sushil Dahal. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
